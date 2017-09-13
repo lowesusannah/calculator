51e9484015d9e59639ed9f286ec3f56842346ae3
Susannah Lowe and Valia Dempsey <valia@>
2017-09-13 16:18:55 -0700

    add HTML and JavaScript code for another function in our calculator: subtraction.

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,18 @@ $(document).ready(function() {
    event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+    var resultAdd = add(number1, number2);
+		$("#outputAdd").text(resultAdd);
+
+	});
+
+	$("form#subtract").submit(function(event) {
+	   event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var resultSubtract = subtract(number1, number2);
+
+		$("#outputSubtract").text(resultSubtract);
   });
+
 });
